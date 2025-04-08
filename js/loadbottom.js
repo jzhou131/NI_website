fc20ce2453cdedc43124f0869049306ec1a89975
jzhou131 <jzhou131@terpmail.umd.edu>
2025-04-08 13:44:39 -0400

    Add files via upload

---
+++ js/loadbottom.js
@@ -0,0 +1,14 @@
+// /js/loadbottom.js
+fetch('/html/bottom.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    document.getElementById('bottom-placeholder').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error loading header:', error);
+  });
